0f9e82b58dd66243e2679fd1d824fc5b36d2d58d
morres83 <morres83@users.noreply.github.com>
2023-03-28 08:14:18 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,6 +2,8 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SERVICE_NAME=$(basename $SCRIPT_DIR)
 
+sed -i '/dbus-mqttevcharger/d' /data/rc.local
+
 rm /service/$SERVICE_NAME
 kill $(pgrep -f 'supervise dbus-mqttevcharger')
 chmod a-x $SCRIPT_DIR/service/run
